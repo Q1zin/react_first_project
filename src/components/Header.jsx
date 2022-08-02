9bd322c5c6b9b24cacf5827e753c239d6dedbdb1
Vladimir <q1zin@mail.ru>
2022-08-02 21:24:26 +0700

    created project structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = ()=>{
+    return (
+        <header className="header">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/1/1e/RPC-JP_Logo.png" alt="logo" className="logo"/>
+            Company name
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <nav className="nav">
+            <a href="#" className="link">Profile</a>
+            <a href="#" className="link">Messages</a>
+            <a href="#" className="link">News</a>
+            <a href="#" className="link">Music</a>
+            <a href="#" className="link">Settings</a>
+        </nav>
+    );
+};
+
+export default NavBar;
